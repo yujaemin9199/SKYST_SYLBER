ae59fd251ceb54be7132bb017de06a21344a38da
carsella0909 <163733902+carsella0909@users.noreply.github.com>
2024-03-23 22:19:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const userRouter = require('./routes/users');
+const flowerRouter = require('./routes/flowers');
+const seedRouter = require('./routes/seeds');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/user', userRouter);
+app.use('/flower', flowerRouter);
+app.use('/seed', seedRouter);
 
 app.use((req, res, next) => {
   res.status(404);
