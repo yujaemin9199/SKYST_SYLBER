c95ae6200558c296dfa64a5300f00bf20698b0d2
Jaemin Yu <yujaemin9199@snu.ac.kr>
2024-03-23 21:59:03 +0900

    register

--- models/userlist.js
+++ models/userlist.js
@@ -1,25 +1,34 @@
 let userlist = [
-    { 
-      id: 'sjb', 
-      pw: 'first note', 
-      
-    }
-  ];
-  
-  exports.list = () => {
-    return userlist.map(({ id, pw }) => ({
-      id,
-      pw,
-    }));
+  { 
+    id: 'sjb', 
+    pw: 'first note', 
+    
   }
-  
-  exports.getpw = (id) => {
-    const usr = userlist.find(
-      (usr) => usr.id === id
-    );
-  
-    if (!usr) {
-      throw new Error('User not found');
-    }
-    return usr.pw;
+];
+
+exports.list = () => {
+  return userlist.map(({ id, pw }) => ({
+    id,
+    pw,
+  }));
+};
+
+exports.getpw = (id) => {
+  const usr = userlist.find(
+    (usr) => usr.id === id
+  );
+
+  if (!usr) {
+    throw new Error('User not found');
+  }
+  return usr.pw;
+};
+
+exports.register = (id, pw) => {
+  const usr = {
+    id,
+    pw,
   };
+  userList.push(usr);
+  return userList;
+};
